e2b7b332ecec0f60f57ce9c9154379a620bc754b
Sarah Quinney <quinney.sarah@gmail.com>
2023-06-15 07:43:09 -0500

    json is watching webpack now and menu.js is writing on the DOM

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,24 +2,20 @@ const utility = require("./utility.js");
 
 function createMenu() {
     utility.clearContent();
-}
 
-//links to content div in index.html//
-let content = document.querySelector("#content");
 
-//Create div for Summer Menu title//
-let menuTitle = document.createElement("div");
+    //links to content div in index.html//
+    let content = document.querySelector("#content");
+
+    //Create div for Summer Menu title//
+    let menuTitle = document.createElement("div");
     menuTitle.id = "menu-title";
     menuTitle.innerText = "Seasonal Summer Menu";
     //append menuTitle to content div//
     content.appendChild(menuTitle);
 
 
-
-
-
-
-
+}
 module.exports = {
-    createMenucontent
+    createMenu
 }
