5df566012f299b2e0258f38822bfcff1d1255f8b
Sarah Quinney <quinney.sarah@gmail.com>
2023-06-15 08:18:01 -0500

    menu.js is in progress

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -14,7 +14,30 @@ function createMenu() {
     //append menuTitle to content div//
     content.appendChild(menuTitle);
 
+    //creates a class div for the macaron menu container content//
+    let macaronMenuContainer = document.createElement("div");
+    macaronMenuContainer.classList.add("macaron-container");
+    content.appendChild(macaronMenuContainer);
 
+    //creates macaron flavors title as a div and attaches it to the macaronMenuContainer//
+    let macaronFlavorsTitle = document.querySelector("div");
+    macaronFlavorsTitle.id = "macaron-flavors-title";
+    macaronFlavorsTitle.innerText = "Macaron Flavors";
+
+
+    //creates macaron allergy info as a div and attaches it to the macaronMenuContainer//
+    let macaronAllergyInfo = document.querySelector("div");
+    macaronAllergyInfo.id = "macaron-allergy-info";
+    macaronAllergyInfo.innerText = "All macarons are gluten and dairy free!";
+
+    //creates the macaron menu for cards to append to//
+    let macaronMenu = document.querySelector("div");
+    macaronMenu.classList.add("macaron-menu");
+
+    //appends all three to the macaronMenuContainer//
+    macaronMenuContainer.appendChild(macaronFlavorsTitle);
+    macaronMenuContainer.appendChild(macaronAllergyInfo);
+    macaronMenuContainer.appendChild(macaronMenu);
 }
 module.exports = {
     createMenu
